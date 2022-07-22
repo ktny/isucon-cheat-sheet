925c562a98417c127f043ac49af65011cb08d7d4
ktny <katsunori.t10@gmail.com>
2022-07-23 00:05:58 +0900

    update

--- scripts/analyze-logs.sh
+++ scripts/analyze-logs.sh
@@ -7,18 +7,15 @@ set -euxo pipefail
 #################################################################
 
 # sshconfig設定済のリモートホスト名を格納
-remote='private-isu'
+web_server='private-isu'
+db_server='private-isu'
 local='/c/users/katsu/Downloads/'
 date=$(date +%Y%m%d-%H%M%S)
 
 # nginxアクセスログ解析
-ssh $remote "cat /var/log/nginx/access.log | alp ltsv --sort=count -r -o ""count,2xx,4xx,5xx,method,uri,min,max,sum,avg"" > /tmp/""$date""-nginx.log"
-
-# 解析結果ログをローカルホストに転送する
+ssh $web_server "cat /var/log/nginx/access.log | alp ltsv --sort=count -r -o ""count,2xx,4xx,5xx,method,uri,min,max,sum,avg"" > /tmp/""$date""-nginx.log"
 scp private-isu:/tmp/"$date"-nginx.log "$local"
 
 # MySQLスローログ解析
-ssh $remote "sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/""$date""-mysql.log"
-
-# 解析結果ログをローカルホストに転送する
+ssh $db_server "sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/""$date""-mysql.log"
 scp private-isu:/tmp/"$date"-mysql.log "$local"
--- scripts/rotate-logs.sh
+++ scripts/rotate-logs.sh
@@ -7,13 +7,14 @@ set -euxo pipefail
 #################################################################
 
 # sshconfig設定済のリモートホスト名を格納
-remote='private-isu'
+web_server='private-isu'
+db_server='private-isu'
 date=$(date +%Y%m%d-%H%M%S)
 
 # nginxアクセスログをローテート
-ssh $remote "sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.""$date"""
-ssh $remote "sudo nginx -s reopen"
+ssh $web_server "sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.""$date"""
+ssh $web_server "sudo nginx -s reopen"
 
 # mysqlスロークエリログをローテート
-ssh $remote "sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.""$date"""
-ssh $remote "sudo mysqladmin flush-logs"
+ssh $db_server "sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.""$date"""
+ssh $db_server "sudo mysqladmin flush-logs"
