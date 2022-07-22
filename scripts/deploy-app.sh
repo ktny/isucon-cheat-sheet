9e215e3cd960693352e07eeb55ccda75f99caaa9
ktny <katsunori.t10@gmail.com>
2022-07-23 00:02:33 +0900

    update

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -3,41 +3,16 @@ set -euxo pipefail
 
 echo "start deploy app ${USER}"
 
-# 配置先
-# sqldest=/home/isucon/webapp/sql/
-# godest=/home/isucon/webapp/go/
-nodedest=/home/isucon/private_isu/webapp/node/
-
 # for server in isu01 isu03; do
-for server in private-isu; do
-    # deploy db
-    # ssh $server "sudo systemctl stop mariadb"
-    # ssh $server "sudo rm -f /var/log/mysql/mysql-slow.log"
-    # ssh $server "mysql -uisucon -pisucon -e'flush logs;' isucondition"
-    # scp mysql.cnf $server:/etc/mysql/conf.d/mysql.cnf
-    # scp webapp/sql/init.sh $server:$sqldest
-    # scp webapp/sql/0_Schema.sql $server:$sqldest
-    # scp webapp/sql/1_InitData.sql $server:$sqldest
-    # ssh $server "/home/isucon/webapp/sql/init.sh"
-    # ssh $server "sudo systemctl start mariadb"
-
-    # deploy nginx
-    # scp nginx.conf $server:/etc/nginx/nginx.conf
-    # ssh $server "sudo rm -f /var/log/nginx/access.log"
-    # ssh $server "sudo systemctl restart nginx"
-
-    # deploy app
-    # ssh $server "sudo systemctl stop isucondition.go.service"
-    # scp webapp/go/main.go $server:$godest
-    # scp webapp/go/go.mod $server:$godest
-    # scp webapp/go/go.sum $server:$godest
-    # ssh $server "cd webapp/go && /home/isucon/local/go/bin/go build -o isucondition"
-    # ssh $server "sudo systemctl start isucondition.go.service"
-    # ssh $server "sudo systemctl restart jiaapi-mock.service"
-
-    # deploy app
-    tsc main.ts
-    scp main.js $server:$nodedest
+for server in isucon11q; do
+    cd ./webapp/nodejs
+    # npm ci
+    tsc
+    cd ../
+    tar zcf isucon.tar.gz ./nodejs
+    ssh $server 'sudo rm -rf /home/isucon/webapp/nodejs/'
+    ssh $server 'tar zxf - -C /home/isucon/webapp/' < isucon.tar.gz
+    ssh $server 'sudo systemctl restart isucondition.nodejs.service'
 done
 
 echo "finish deploy app ${USER}"
