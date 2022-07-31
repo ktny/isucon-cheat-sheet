1b38505a7899ea49f87ea13b8c2908d38311c3b1
ktny <katsunori.t10@gmail.com>
2022-07-31 15:38:21 +0900

    update

--- scripts/analyze-logs.sh
+++ scripts/analyze-logs.sh
@@ -7,15 +7,15 @@ set -euxo pipefail
 #################################################################
 
 # sshconfig設定済のリモートホスト名を格納
-web_server='private-isu'
-db_server='private-isu'
+web_server='isu01'
+db_server='isu01'
 local='/c/users/katsu/Downloads/'
 date=$(date +%Y%m%d-%H%M%S)
 
 # nginxアクセスログ解析
-ssh $web_server "cat /var/log/nginx/access.log | alp ltsv --sort=count -r -o ""count,2xx,4xx,5xx,method,uri,min,max,sum,avg"" > /tmp/""$date""-nginx.log"
-scp private-isu:/tmp/"$date"-nginx.log "$local"
+ssh $web_server "cat /var/log/nginx/access.log | alp ltsv --sort=sum -r -m "/api/player/competition/.+/ranking,/api/organizer/competition/.+/score,/api/organizer/competition/.+/finish,/api/player/player/.+,/api/organizer/player/.+/disqualified" -o ""count,2xx,4xx,5xx,method,uri,min,max,sum,avg"" > /tmp/""$date""-nginx.log"
+scp $web_server:/tmp/"$date"-nginx.log "$local"
 
 # MySQLスローログ解析
 ssh $db_server "sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/""$date""-mysql.log"
-scp private-isu:/tmp/"$date"-mysql.log "$local"
+scp $db_server:/tmp/"$date"-mysql.log "$local"
--- scripts/rotate-logs.sh
+++ scripts/rotate-logs.sh
@@ -7,8 +7,8 @@ set -euxo pipefail
 #################################################################
 
 # sshconfig設定済のリモートホスト名を格納
-web_server='private-isu'
-db_server='private-isu'
+web_server='isu01'
+db_server='isu01'
 date=$(date +%Y%m%d-%H%M%S)
 
 # nginxアクセスログをローテート
@@ -17,4 +17,6 @@ ssh $web_server "sudo nginx -s reopen"
 
 # mysqlスロークエリログをローテート
 ssh $db_server "sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.""$date"""
-ssh $db_server "sudo mysqladmin flush-logs"
+ssh $db_server "sudo systemctl restart mysql"
+# ssh $db_server "mysqladmin flush-logs"
+# ssh $db_server "mysql -uisucon -pisucon -e'flush logs;' isports"
